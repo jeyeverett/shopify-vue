90cf4a04f7a39c055c9d0800f728973526defbd0
Jeysen Freedman <jeysenfreedman@gmail.com>
2021-11-09 12:43:56 -0400

    transform product-card into vue component

--- assets/application.js
+++ assets/application.js
@@ -5,6 +5,7 @@ const store = Vue.reactive({
       items: [],
       hidden: true,
     },
+    products: {},
     menu: {
       hidden: true,
     },
@@ -15,6 +16,37 @@ const store = Vue.reactive({
       const response = await fetch("/cart.js");
       const cartData = await response.json();
       this.state.cart.items = cartData;
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  async addToCart(data) {
+    try {
+      const response = await fetch("/cart/add.js", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (!response.ok) {
+        // throw error
+      }
+
+      //refresh the cart (store.getCart() is defined in application.js) then open it
+      await this.getCart();
+      this.toggleCart();
+    } catch (err) {
+      console.log(err);
+    }
+  },
+
+  async getProductData(productHandle) {
+    try {
+      const response = await fetch(`/products/${productHandle}.js`);
+      return await response.json();
     } catch (err) {
       console.log(err);
     }
